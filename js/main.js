ecc23cbbf729338266a5828029dccde5c7c8a74c
adamianniello <adam.ianniello@gmail.com>
2017-02-19 20:56:31 -0800

    batch upload
    first coding upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+var images = [
+    "images/intro/img_1.jpg",
+    "images/intro/img_2.jpg",
+    "images/intro/img_3.jpg",
+    "images/intro/img_4.jpg"];
+
+function randImg() {
+    var size = images.length
+    var x = Math.floor(size * Math.random())
+    document.getElementById('image').src = images[x];
+}
+
+randImg();
+
+});
